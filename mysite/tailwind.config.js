8c8b4cb2e6e3dbc3d297d9f78147df4e2a42c92f
Danila <idanila331@gmail.com>
2025-01-07 18:15:04 +0200

    navbar.html created

--- mysite/tailwind.config.js
+++ mysite/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./myapp/**/*.{html,js}"],
+  content: ["./myapp/**/*.{html,js}", "./users/**/*.{html,js}",],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
